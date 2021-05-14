bfe80be7fb0a80da2f4d1173341455c5be488da0
gfgullo <gfgullo@gmail.com>
2021-05-14 18:50:37 +0200

    update

--- pages/strings.js
+++ pages/strings.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import LocalizedStrings from 'react-localization';
 
-export let strings = new LocalizedStrings({
+export default strings = new LocalizedStrings({
 
   en: {
     heroTitle:"Giuseppe Gullo",
